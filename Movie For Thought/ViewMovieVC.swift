c1c55ac4459f07f5ee5f9d211b217f8c0bb661b3
ankhangkieu <ankhangkieu@gmail.com>
2017-01-22 01:11:10 -0800

    Interface for View Movie Screen

---
+++ Movie For Thought/ViewMovieVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewMovieVC.swift
+//  Movie For Thought
+//
+//  Created by Ak Kieu on 1/22/17.
+//  Copyright © 2017 Ak Kieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewMovieVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
